64f989067a8075f3d5d8f9aa64573b2041ba93c8
George Busby <georgebusby08@gmail.com>
2025-02-04 15:40:47 -0700

    All variables for upgrades and technologies are now set

--- upgrades/upgrade-base.py
+++ upgrades/upgrade-base.py
@@ -1,4 +1,4 @@
-#variables:
+#variables for basic unlocks:
 copperOreUnlocked = False
 refinedCopperUnlocked = False
 copperPlateUnlocked = False
@@ -7,3 +7,76 @@ wiresUnlocked = False
 advancedWiresUnlocked = False
 eliteWiresUnlocked = False
 rawCrystalUnlocked = False
+crystalUnlocked = False
+activatedCrystalUnlocked = False
+crystalDustUnlocked = False
+oilUnlocked = False
+heavyOilUnlocked = False
+lightOilUnlocked = False
+plasticUnlocked = False
+waterUnlocked = False
+ironOreUnlocked = False
+refinedIronUnlocked = False
+ironPlateUnlocked = False
+ironRodUnlocked = False
+
+#variables for mid unlocks:
+compressedCrystalUnlocked = False
+activatedCompressedCrystalUnlocked = False
+crystalMoldUnlocked = False
+advancedBaseUnlocked = False
+eliteBaseUnlocked = False
+circuitBaseUnlocked = False
+ultimateBaseUnlocked = False
+
+#variables for high unlocks:
+compressedCrystalDustUnlocked = False
+realityCrystalUnlocked = False
+realityAshesUnlocked = False
+ultimateWiresUnlocked = False
+
+#circuit unlocks:
+circuit1Unlocked = False
+circuit1_5Unlocked = False
+circuit2Unlocked = False
+circuit2_5Unlocked = False
+circuit3Unlocked = False
+circuit4Unlocked = False
+circuit5Unlocked = False
+circuit5_5Unlocked = False
+circuit6Unlocked = False
+circuit7Unlocked = False
+circuit8Unlocked = False
+circuit8_5Unlocked = False
+circuit9Unlocked = False
+circuit10Unlocked = False
+circuitloopingUnlocked = False
+highestCircuitLoopUnlocked = 10
+
+#even more endgame stuffs:
+reallyBigUpgradeThatEndsTheGameUnlocked = False
+logistics1Unlocked = False
+logistics2Unlocked = False
+logistics3Unlocked = False
+teleportersUnlocked = False
+
+#upgrades:
+betterPowerUnlocked = False
+evenBetterPowerUnlocked = False
+americaPowerUnlocked = False
+nuclearPowerUnlocked = False
+magicPowerUnlocked = False
+fracturedPowerUnlocked = False
+improvedResearchUnlocked = False
+nuclearResearchUnlocked = False
+timeWarpUnlocked = False
+advertisingUnlocked = False
+superAdvertisingUnlocked = False
+circuitBasedReligionUnlocked = False
+brainwashingUnlocked = False
+stockTradingUnlocked = False
+fluidsUnlocked = False
+expandableFactoryUnlocked = False
+advancedOilProcessingUnlocked = False
+moreMinesUnlocked = False
+rodsUnlocked = False
