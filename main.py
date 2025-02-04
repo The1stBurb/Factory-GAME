050b9c5a7437cc5161893d22ff61a05005047f9b
The1stBurb <eli.cooper@ucas-edu.net>
2025-02-03 19:58:37 -0700

    added imgs and sfx

--- main.py
+++ main.py
@@ -3,21 +3,37 @@ from keep import *
 import pygameBases as pb
 #comments should consist of, what does this do, why is it here, when is it used
 pygame.init()
+pygame.mixer.init()
 X = 1000
 Y = 1000
 screen = pygame.display.set_mode((X, Y),pygame.RESIZABLE)#,pygame.FULLSCREEN)
 clk=pygame.time.Clock()
 pygame.font.init()
 font = pygame.font.SysFont(None, 10)
-#this is for ease of use terms of pygame displaying stuff
-    
-
+#runs SFX duh
+class SFXRunner:
+    def __init__(self):
+        self.ambient=[pygame.mixer.Sound('sfx\\back1.wav'),pygame.mixer.Sound('sfx\\back2.wav'),pygame.mixer.Sound('sfx\\back3.wav'),
+                      pygame.mixer.Sound('sfx\\back4.wav'),pygame.mixer.Sound('sfx\\back5.wav'),pygame.mixer.Sound('sfx\\back6.wav'),]
+        self.ambOn=random.random(0,len(self.ambient)-1)
+        # self.msOnClk=pygame.mixer.Sound('sound_file.wav')
+        # self.msYes=pygame.mixer.Sound('sound_file.wav')
+        # self.msNo=pygame.mixer.Sound('sound_file.wav')
+    def runAmb(self):
+        if not pygame.mixer.music.get_busy():
+            self.ambOn=random.random(0,len(self.ambient)-1)
+            self.ambient[self.ambOn].play()
+sfx=SFXRunner()
 #size of images
 sz=100
 #list of image for ease of use and stuff lol
-imgs=[
-    pb.imgGit("img\\blank.png",sz,sz),pb.imgGit("img\\refiner-1.png",sz,sz),
-]
+imgs=[]#{"build":[],"belt":[],"icon":[],"item":[],"other":[]}
+#raw for easy convert
+rawImg={"building":["CuCond","FeCond","Sell"]}
+#raw to img convert
+for i in rawImg:
+    for j in rawImg[i]:
+        imgs.append(pb.imgGit(f"img\\{i}\\{j}.png",sz,sz),)
 #grid of blocks
 grid=[]
 #self explanitory
@@ -59,3 +75,5 @@ while True:
         for x,j in enumerate(i):
             pb.image(screen,imgs[j],x*sz+grd[0],y*sz+grd[1])
     pygame.display.flip()
+
+    sfx.runAmb()
