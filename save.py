4c286cfa3257e5f82d6d3d26ec9c4019fd398edd
The1stBurb <eli.cooper@ucas-edu.net>
2025-03-03 20:30:30 -0700

    i have problems guys

--- save.py
+++ save.py
@@ -1,2 +1,8 @@
+from save_code.comPile import runPiler
 #this runs the whole save stuff, it smart
 saveNum=2
+def clearSave(saveOver=saveNum):
+    runPiler(saveOver,"",encode=False)
+def clearAll():
+    for i in range(5):
+        clearSave(i+1)
